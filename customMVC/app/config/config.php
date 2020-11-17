b3271665917e813ab501754188c2a96a0852052f
Aleksa <66486619+lexatexas@users.noreply.github.com>
2020-11-17 20:30:48 +0100

    Add files via upload

---
+++ customMVC/app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'test');
+
+
+   define('APPROOT', dirname(dirname(__FILE__)));
+
+   define('URLROOT', 'http://localhost/customMVC');
+
+   define('SITENAME', 'MVC Framework');
+?> 
