40b5c9afe9d5510df21592fdb47e79d597363af1
Aleksa <66486619+lexatexas@users.noreply.github.com>
2020-11-17 20:41:28 +0100

    Add files via upload

---
+++ customMVC/app/views/pages/index.php
@@ -0,0 +1,11 @@
+<?php require_once APPROOT.'/views/inc/header.php'; ?>
+
+<?php
+
+echo '<h1>'.$data['title']. '</h1></br>';
+echo $data['desc'];
+
+
+?>
+
+<?php require_once APPROOT.'/views/inc/footer.php'; ?>
